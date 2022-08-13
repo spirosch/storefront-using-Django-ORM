3e6aa77b95282ba57ef79daffdcb44a4217f568b
Spiros <geokh@hotmail.com>
2022-08-13 12:27:36 +0300

    Complex Lookups Using Q Objects

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,20 @@
+from asyncio.windows_events import NULL
+import collections
+from genericpath import exists
+from itertools import product
+from math import prod
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
+from django.db.models import Q
+from store.models import Collection, Customer, Order, OrderItem, Product
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+    queryset = Product.objects.filter(Q(inventory__lt=10)) | ~Q(unit_price__lt=20)
+    
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
